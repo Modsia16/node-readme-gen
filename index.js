8d29073b58ca615344804d607241ffa8b7870b28
Modsia16 <84951325+Modsia16@users.noreply.github.com>
2021-10-19 11:51:11 -0700

    adding utils and updating app

--- index.js
+++ index.js
@@ -1,78 +1,83 @@
-//node modules
-const fs = require('fs')
-const inq = require('inquirer')
+//node modules needed for applications
+const inquirer = require('inquirer')
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-//quesitons
-inq.prompt([
-    {
-        type: 'input',
-        message: "What is the project title?",
-        name: "title",
-        validate: (value)=>{if(value){return true} else{ return 'needs a value to move onward'}},
-    },
-    {
-        type: 'input',
-        message: "how do you install?",
-        name: "installation",
-        validate: (value)=>{if(value){return true} else{ return 'needs a value to move onward'}},
-    },
-    {
-        type: 'input',
-        message: "instructions to follow?",
-        name: "instructions",
-        validate: (value)=>{if(value){return true} else{ return 'needs a value to move onward'}},
-    },
-    {
-        type: 'input',
-        message: "credentials?",
-        name: "credits",
-        validate: (value)=>{if(value){return true} else{ return 'needs a value to move onward'}},
-    },
-    {
-        type: 'input',
-        message: "how do you use your app?",
-        name: "usage",
-        validate: (value)=>{if(value){return true} else{ return 'needs a value to move onwaard'}},
-    },
-    {
-        type: 'list',
-        message: "licences?",
-        name: "licences",
-        choices: ['MIT', 'GPL', 'Apache', 'GNU', 'N/A'],
-        validate: (value)=>{if(value){return true} else{ return 'needs a value to move onward'}},
-    },
-    {
-        type: 'input',
-        message: "how do you use your app?",
-        name: "confirm",
-        validate: (value)=>{if(value){return true} else{ return 'needs a value to move onward'}},
-    },
-    {
-        type: 'input',
-        message: 'GitHib username:',
-        name: 'git',
-        validate: (value)=>{if(value){return true} else{ return 'needs a value to move onward'}},
-    },
-    {
-        type: 'input',
-        message: 'Email:',
-        name: 'email',
-        validate: (value)=>{if(value){return true} else{ return 'needs a value to move onward'}},
-    }
-]) .then(({
-    title,
-    installation,
-    instructions,
-    credit,
-    licence,
-    git,
-    linkedin,
-    email,
-    usage,
-    contribution
-})=>{
-    const template = `# ${title}
-    `;
-})
+//quesitons array
+const questions = [
+        {
+            type: 'input',
+            message: "What is the project title?",
+            name: "title",
+            validate: (value)=>{if(value){return true} else{ return 'needs a value to move onward'}},
+        },
+        {
+            type: 'input',
+            message: "Briefly describe your project?",
+            name: "description",
+            validate: (value)=>{if(value){return true} else{ return 'needs a value to move onward'}},
+        },
+        {
+            type: 'input',
+            message: "What are the instructions to install your application?",
+            name: "installation",
+            validate: (value)=>{if(value){return true} else{ return 'needs a value to move onward'}},
+        },
+        {
+            type: 'input',
+            message: "Enter usage instructions?",
+            name: "usage",
+            validate: (value)=>{if(value){return true} else{ return 'needs a value to move onward'}},
+        },
+        {
+            type: 'input',
+            message: "Enter the projects contributor guidelines?",
+            name: "contributing",
+            validate: (value)=>{if(value){return true} else{ return 'needs a value to move onward'}},
+        },
+        {
+            type: 'input',
+            message: "Enter your porject test instructions",
+            name: "test",
+            validate: (value)=>{if(value){return true} else{ return 'needs a value to move onwaard'}},
+        },
+        {
+            type: 'list',
+            message: "Which licence will your project be under?",
+            name: "licence",
+            choices: ['MIT', 'Apache', 'GPL'],
+            validate: (value)=>{if(value){return true} else{ return 'needs a value to move onward'}},
+        },
+        {
+            type: 'input',
+            message: 'What is your GitHib username?',
+            name: 'username',
+            validate: (value)=>{if(value){return true} else{ return 'needs a value to move onward'}},
+        },
+        {
+            type: 'input',
+            message: 'What is you email address?',
+            name: 'email',
+            validate: (value)=>{if(value){return true} else{ return 'needs a value to move onward'}},
+        },
+    ];
 
-    
+//function for readme file
+function writeTofile(fileName, data) {
+    const mdPageContent = generateMarkdown(data);
+    fs.writeFile(fileName, mdPageContent, (err) =>
+    err ? console.error(err) : console.log('success')
+    );
+}
+
+//function to initilaize app
+function init() {
+    inquirer.prompt(questions)
+    .then((data) => {
+        const fileName = `README.md`;
+        writeToFile(fileName, data)
+    })
+}
+
+//function to call to initilize app
+init();
